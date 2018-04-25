fde6a0713a0f0a797476ee25a5e0c26feedd9ad4
Benoit Foucher <benoit.foucher@epsi.fr>
2018-04-25 11:10:24 +0200

    Javascript angle dorit en fonction de l'ordre

--- views/templates/form.php
+++ views/templates/form.php
@@ -1,4 +1,46 @@
-<form class="row align-items-center" method="post" action="index.php">
+    <script type="text/javascript">
+        function selectOrdre(ordre) {
+        var select = document.getElementById("ordre");
+        var valeur = select.options[0].value;
+        if (valeur==" "){
+            select.remove(0);
+        };
+        var angle = document.getElementById("angles-droit");
+        angle.remove(5);
+        angle.remove(4);
+        angle.remove(3);
+        angle.remove(2);
+        angle.remove(1);
+        angle.remove(0);
+        var option = document.createElement("option");
+        if (ordre==3) {
+            var option1 = document.createElement("option");
+            var option0 = document.createElement("option");
+            option0.text = "0";
+            option1.text = "1";
+            angle.add(option0);
+            angle.add(option1);
+        };
+        if (ordre==4 || ordre==5) {
+            var option = document.createElement("option");
+            var option2 = document.createElement("option");
+            var option3 = document.createElement("option");
+            var option4 = document.createElement("option");
+            var option5 = document.createElement("option");
+            option.text = "1";
+            option2.text = "2";
+            option3.text = "3";
+            option4.text = "4";
+            option5.text = "0";
+            angle.add(option5);
+            angle.add(option);
+            angle.add(option2);
+            angle.add(option3);
+            angle.add(option4);
+        };
+        }
+    </script>
+    <form class="row align-items-center" method="post" action="index.php">
     <div class="col-12">
         <div class="row align-items-center bloc">
             <div class="col-12 col-md-6 col-lg-3 text-center">
@@ -7,7 +49,7 @@
                         <label>Nombre de côtés :</label>
                     </div>
                     <div class="col-12">
-                        <select name="ordre">
+                        <select name="ordre" id="ordre" onchange="selectOrdre(this.value)">
                             <option value="3">3</option>
                             <option value="4">4</option>
                             <option value="5">5</option>
@@ -21,7 +63,8 @@
                         <label>Angles droits :</label>
                     </div>
                     <div class="col-12">
-                        <input type="number" name="angles-droits">
+                        <select id="angles-droit" name="angles-droit">
+                        </select>
                     </div>
                 </div>
             </div>
