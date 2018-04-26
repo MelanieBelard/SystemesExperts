59adfc7da5d46a1ee9de471865b4ff4ea884828b
Mélanie <belard.melanie@gmail.com>
2018-04-26 16:01:31 +0200

    commit

--- index.php
+++ index.php
@@ -11,19 +11,16 @@
     if (isset($_POST['ordre']) && isset($_POST['angles-droits']) && isset($_POST['cotes-egaux']) && isset($_POST['cotes-paralleles'])) {
 
         $response = "OK";
-        $ordre['value']         = strval($_POST['ordre']);
-        $anglesDroits['value']  = strval($_POST['angles-droits']);
-        $cotesEgaux['value']    = strval($_POST['cotes-egaux']);
-        $cotesPara['value']     = strval($_POST['cotes-paralleles']);
+        $ordre['value']         = ($_POST['ordre'] == null) ? "0" : $_POST['ordre'];
+        $anglesDroits['value']  = ($_POST['angles-droits'] == null) ? "0" : $_POST['angles-droits'];
+        $cotesEgaux['value']    = ($_POST['cotes-egaux'] == null) ? "0" : $_POST['cotes-egaux'];
+        $cotesPara['value']     = ($_POST['cotes-paralleles'] == null) ? "0" : $_POST['cotes-paralleles'];
 
         $csvLine = "";
         foreach ([$ordre, $anglesDroits, $cotesEgaux, $cotesPara] as $key) {
-            if ($key['value'] == null) {
-                $key['value'] = "0";
-                // var_dump:($key);
-            }
             $csvLine .= $key['value'].';';
         }
+        // echo $csvLine;
         // On va ensuite chercher si la même ligne existe dans le CSV
         // Si oui, on va dans model
         // Si non, on va dans controller et on utilise l'algo
